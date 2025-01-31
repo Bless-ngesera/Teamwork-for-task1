adb1ff7fd1ece7f923680f345bf4e9f087311b3b
Bless-ngesera <164491766+Bless-ngesera@users.noreply.github.com>
2025-01-31 16:50:38 +0300

    finalCommit
    Complete code

---
+++ MusicCatalogue.java
@@ -0,0 +1,19 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+public class MusicCatalogue {
+    public  List<MusicEntry> entries;
+
+    public MusicCatalogue() {
+        entries = new ArrayList<>();
+    }
+
+    public void addEntry(MusicEntry entry) {
+        entries.add(entry);
+    }
+
+    public List<MusicEntry> getEntries() {
+        return entries;
+    }
+}
