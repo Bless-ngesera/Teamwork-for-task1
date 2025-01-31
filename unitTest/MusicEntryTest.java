2034aa6f57ac443940126d23d2823296b12b9c55
Bless-ngesera <164491766+Bless-ngesera@users.noreply.github.com>
2025-01-31 14:54:20 +0300

    CommitTest4
    From line 6 to line 20 we have declared a class called MusicEntryTest and we test the class using two methods: -testConstructor and
    -testToString...

--- unitTest/MusicEntryTest.java
+++ unitTest/MusicEntryTest.java
@@ -3,3 +3,18 @@ package Unknown;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+public class MusicEntryTest {
+
+    @Test
+    public void testConstructor() {
+        MusicEntry entry = new MusicEntry("Artist1", "Studio1", "Rap", true);
+    }
+
+    @Test
+    public void testToString() {
+
+        MusicEntry entry = new MusicEntry("Artist1", "Studio1", "Rap", true);
+        String expectedString = "Artist: Artist1, Studio: Studio1, Category: Rap, Available: true";
+        assertEquals(expectedString, entry.toString(), "toString output should match");
+    }
+}
