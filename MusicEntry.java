3c90579cdaf7003ee3088864822501f2e371abbe
Bless-ngesera <164491766+Bless-ngesera@users.noreply.github.com>
2025-01-31 10:40:38 +0300

    Commit5
    I have declared 4 variables, created a constructor to initialize those variables, and override toString method...

--- MusicEntry.java
+++ MusicEntry.java
@@ -1,1 +1,18 @@
+public class MusicEntry {
+    private String artistName;
+    private String recordingStudio;
+    private String category;
+    private boolean available;
 
+    public MusicEntry(String artistName, String recordingStudio, String category, boolean available) {
+        this.artistName = artistName;
+        this.recordingStudio = recordingStudio;
+        this.category = category;
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist: " + artistName + ", Studio: " + recordingStudio + ", Category: " + category + ", Available: " + available;
+    }
+}
