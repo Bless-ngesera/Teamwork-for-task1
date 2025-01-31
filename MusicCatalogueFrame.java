919d8dd3efa9f570fe3f699956f62dfe49804610
Bless-ngesera <164491766+Bless-ngesera@users.noreply.github.com>
2025-01-31 10:56:01 +0300

    Commit7
    There was some small error on the line 7, and we've fixed it

--- MusicCatalogueFrame.java
+++ MusicCatalogueFrame.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MusicCatalogueFrame extends JFrame {
-    private JT'extField artistField;
+    private JTextField artistField;
     private JTextField studioField;
     private JComboBox<String> categoryComboBox;
     private JCheckBox availableCheckBox;
