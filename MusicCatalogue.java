0ccf856775f3b362d0ab00346fc6a590e534dc3d
Wasswa Ishaq <ngeserabless@gmail.com>
2025-01-30 23:56:06 -0800

    MusicCatalogue
    On line 5 l have a method Music Catalogue, on line 9 l have a void method add entry and on line 13 we have a method get entries and the class ends

--- MusicCatalogue.java
+++ MusicCatalogue.java
@@ -2,3 +2,15 @@ import java.util.ArrayList;
 import java.util.List;
 public class MusicCatalogue {
     private List<MusicEntry> entries;
+        public MusicCatalogue() {
+        entries = new ArrayList<>();
+    }
+
+    public void addEntry(MusicEntry entry) {
+        entries.add(entry);
+    }
+
+    public List<MusicEntry> getEntries() {
+        return entries;
+    }
+}
