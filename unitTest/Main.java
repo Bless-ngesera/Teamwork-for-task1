271355e319cf4c045876d0723005d03e94d4f166
Bless-ngesera <164491766+Bless-ngesera@users.noreply.github.com>
2025-01-31 15:10:17 +0300

    CommitTest7
    We have created the Main class

---
+++ unitTest/Main.java
@@ -0,0 +1,12 @@
+package Unknown;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MusicCatalogueFrame frame = new MusicCatalogueFrame();
+            frame.setVisible(true);
+        });
+    }
+}
